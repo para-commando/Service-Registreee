af597abe6f553d3ca3c0d549775734d60eb42ab5
para-commando <anirudh.nayak.172k@gmail.com>
2024-09-04 23:38:15 +0530

    config changes

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), {
+    name: 'custom-startup-message',
+    configureServer(server) {
+      server.httpServer.once('listening', () => {
+        const address = server.httpServer.address();
+        const host = address.address === '::' ? 'localhost' : address.address;
+        const port = address.port;
+        console.log(`🚀 Service Registree is Live at: http://${host}:${port}/login`);
+      });
+    },
+  },],
 })
